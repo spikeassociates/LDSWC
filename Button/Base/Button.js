fcf0d81b29195aee144d99fef1b61e221db23a17
Joe Bordes <joe@tsolucio.com>
2019-04-22 16:55:40 +0200

    feat(Button)

---
+++ Button/Base/Button.js
@@ -0,0 +1,92 @@
+import { LitElement, html } from '../../libs/lit-element/lit-element.js';
+import {joinClassNames} from '../../libs/ldswcutils/ldswcutils.js';
+import {ldswcconfig} from '../../ldswcconfig.js';
+import ButtonIcon from './ButtonIcon.js';
+
+export default class Button extends LitElement {
+	static get properties() {
+		return {
+			/**
+			 * class name
+			 */
+			className: { type: String },
+			/**
+			 * icon name
+			 */
+			icon: { type: String }, // isRequired
+			/**
+			 * icon sprite name
+			 */
+			sprite: { type: String }, // isRequired
+			/**
+			 * Title attribute. Will be button content if no children are set
+			 */
+			title: { type: String },
+			/**
+			 *  Shortcut to render a button with an icon. Can be "left" or "right". Used together with `icon`
+			 */
+			iconPosition: { type: String },
+			/**
+			 * Can be either `neutral`, `brand`, `outline-brand`, `destructive`, `text-destructive`, `success` or `inverse`
+			 * Default to `neutral`, set to "none" or `null` explicitely to render a plain button
+			 */
+			flavor: { type: String },
+			/**
+			 * Optional href, renders as `a` when set
+			 */
+			href: { type: String },
+		}
+	}
+
+	constructor() {
+		super();
+		this.className = null;
+		this.icon = null;
+		this.iconPosition = null;
+		this.sprite = 'standard';
+		this.title = null;
+		this.flavor = 'neutral';
+		this.href = null;
+	}
+
+	get displayName() {
+		return 'Button';
+	}
+
+	get innerButtonIcon() {
+		return html`<ldswc-buttonicon position=${this.position} icon=${this.icon} sprite=${this.sprite}></ldswc-buttonicon></ldswc-buttonicon>`;
+	}
+
+	getRightShortcut(isRightShortcut, isShortcut) {
+		if (isRightShortcut) {
+			return html`<slot></slot>${isShortcut && this.innerButtonIcon}`;
+		} else {
+			return html`${isShortcut && this.innerButtonIcon}<slot></slot>`;
+		}
+	}
+
+	render() {
+		//const otherattrs = getRestOfAttribs(this.attributes, this.constructor.properties);
+		const hasFlavor = this.flavor != null && this.flavor !== 'none';
+		const sldsClasses = [
+			'slds-button',
+			{ [`slds-button_${this.flavor}`]: hasFlavor },
+			this.className
+		];
+		const position = this.iconPosition || 'left';
+		const isShortcut = !!this.icon && !!this.sprite;
+		const isRightShortcut = isShortcut && position !== 'left';
+
+		return html`
+<style>
+@import '${ldswcconfig.ldsBasePath}/styles/salesforce-lightning-design-system.css';
+</style>
+${this.href ?
+	html`<a class=${joinClassNames(sldsClasses)} href=${this.href} title=${this.title}>${this.getRightShortcut(isRightShortcut, isShortcut)}</a>`:
+	html`<button class=${joinClassNames(sldsClasses)} href=${this.href} title=${this.title}>${this.getRightShortcut(isRightShortcut, isShortcut)}</button>`
+}
+`;
+	}
+}
+
+customElements.define('ldswc-button', Button);
---
+++ Button/Base/ButtonIcon.js
@@ -0,0 +1,65 @@
+import { LitElement, html } from '../../libs/lit-element/lit-element.js';
+import {joinClassNames} from '../../libs/ldswcutils/ldswcutils.js';
+import IconSVG from '../../Icon/IconSVG.js';
+import {ldswcconfig} from '../../ldswcconfig.js';
+
+export default class ButtonIcon extends LitElement {
+	static get properties() {
+		return {
+			/**
+			 * Optional additional className
+			 */
+			className: { type: String },
+			/**
+			 * icon name
+			 */
+			icon: { type: String }, // isRequired
+			/**
+			 * Position in `Button`. Either left or right, can also be `null`
+			 */
+			position: { type: String },
+			/**
+			 * icon size
+			 */
+			size: { type: String }, // oneOf(['xx-small', 'x-small', 'small', 'medium', 'large']),
+			/**
+			 * icon sprite name
+			 */
+			sprite: { type: String }, // isRequired
+		}
+	}
+
+	constructor() {
+		super();
+		this.className = null;
+		this.icon = '';
+		this.position = null;
+		this.size = 'small';
+		this.sprite = 'standard';
+	}
+
+	render() {
+		//const otherattrs = getRestOfAttribs(this.attributes, this.constructor.properties);
+		
+		const sldsClasses = [
+			'slds-button__icon',
+			{ [`slds-button__icon_${this.size}`]: !!this.size },
+			{ [`slds-button__icon_${this.position}`]: !!this.position },
+			this.className
+		];
+
+		return html`
+<style>
+@import '${ldswcconfig.ldsBasePath}/styles/salesforce-lightning-design-system.css';
+</style>
+<ldswc-iconsvg
+	className=${joinClassNames(this.sldsClasses)}
+	isButton
+	icon=${this.icon}
+	sprite=${this.sprite}
+></ldswc-iconsvg>
+`;
+	}
+}
+
+customElements.define('ldswc-buttonicon', ButtonIcon);
