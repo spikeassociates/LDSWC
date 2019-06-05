4bf0e4dab2ab55450e76481ed6388e337cfebb22
Joe Bordes <joe@tsolucio.com>
2019-06-06 00:19:17 +0200

    feat(PageHeader) Adding web component

---
+++ PageHeader/PageHeader.js
@@ -0,0 +1,80 @@
+import { LitElement, html } from '../libs/lit-element/lit-element.js';
+import { joinClassNames } from '../libs/ldswcutils/ldswcutils.js';
+import { ldswcconfig } from '../ldswcconfig.js';
+import Icon from '../Icon/Icon.js';
+import MediaObject from '../MediaObject/MediaObject.js';
+
+export default class PageHeader extends LitElement {
+	static get properties() {
+		return {
+			/**
+			 * className
+			 */
+			className: { type: String },
+			/**
+			 * icon
+			 */
+			icon: { type: String },
+			/**
+			 * sprite
+			 */
+			sprite: { type: String },
+			/**
+			 * info subtitle
+			 */
+			info: { type: String },
+			/**
+			 * title
+			 */
+			title: { type: String },
+		}
+	}
+
+	constructor() {
+		super();
+		this.checked = null;
+		this.icon = '';
+		this.sprite = '';
+		this.info = '';
+		this.title = '';
+	}
+
+	render() {
+		const sldsClasses = [
+			'slds-page-header',
+			this.className,
+		];
+
+		return html`
+			<style>
+				@import '${ldswcconfig.ldsBasePath}/styles/salesforce-lightning-design-system.css';
+			</style>
+			<div class=${joinClassNames(sldsClasses)}>
+				<div class="slds-page-header__row">
+					<div class="slds-page-header__col-title">
+						<ldswc-mediaobject figurePosition="left" title=${this.title}>
+							<ldswc-icon
+								sprite=${this.sprite}
+								icon=${this.icon}
+								svgClassName="slds-page-header__icon"
+								className="slds-icon-standard-${this.icon}"
+								slot="figure"
+							></ldswc-icon>
+							<span slot="body">
+								<div class="slds-page-header__name">
+									<div class="slds-page-header__name-title">
+										<h1>
+										<span class="slds-page-header__title slds-truncate" title="${this.title}">${this.title}</span>
+										</h1>
+									</div>
+								</div>
+								<p class="slds-page-header__name-meta">${this.info}</p>
+							</span>
+						</ldswc-mediaobject>
+					</div>
+				</div>
+			</div>`;
+	};
+}
+
+customElements.define('ldswc-pageheader', PageHeader);
---
+++ PageHeader/index.js
@@ -0,0 +1,5 @@
+import ObjectHome from './ObjectHome.js';
+import PageHeaderBase from './PageHeaderBase.js';
+import RecordHome from './RecordHome.js';
+
+export { ObjectHome, PageHeaderBase, RecordHome };
