c8c933495bc423c3e1419d13cfa9a67b493bd9a6
joebordes <joe@tsolucio.com>
2019-12-09 14:05:03 +0100

    global include file

---
+++ ldsincs.php
@@ -0,0 +1,45 @@
+<script>
+if (typeof window.ldswcproperties=='undefined') {
+	window.ldswcproperties = {};
+}
+</script>
+<script type="module" src="./include/ldswc/ClickOutside/ClickOutside.js"></script>
+<script type="module" src="./include/ldswc/Icon/Icon.js"></script>
+<script type="module" src="./include/ldswc/Icon/ScoreIcon.js"></script>
+<script type="module" src="./include/ldswc/Icon/StrengthIcon.js"></script>
+<script type="module" src="./include/ldswc/Icon/EqIcon.js"></script>
+<script type="module" src="./include/ldswc/Spinner/index.js"></script>
+<script type="module" src="./include/ldswc/Button/Base/Button.js"></script>
+<script type="module" src="./include/ldswc/Button/Base/ButtonIcon.js"></script>
+<script type="module" src="./include/ldswc/Button/IconButtons/IconButton.js"></script>
+<script type="module" src="./include/ldswc/Button/IconButtons/StatefulIconButton.js"></script>
+<script type="module" src="./include/ldswc/Button/FormButtons/CheckboxButton.js"></script>
+<script type="module" src="./include/ldswc/Button/ButtonGroups/ButtonGroup.js"></script>
+<script type="module" src="./include/ldswc/Badge/Badge.js"></script>
+<script type="module" src="./include/ldswc/MediaObject/MediaObject.js"></script>
+<script type="module" src="./include/ldswc/Timeline/index.js"></script>
+<script type="module" src="./include/ldswc/PageHeader/PageHeader.js"></script>
+<script type="module" src="./include/ldswc/Accordion/index.js"></script>
+<script type="module" src="./include/ldswc/SummaryDetail/index.js"></script>
+<script type="module" src="./include/ldswc/Popover/index.js"></script>
+<script type="module" src="./include/ldswc/ExpandableSection/index.js"></script>
+<script type="module" src="./include/ldswc/Menu/index.js"></script>
+<script type="module" src="./include/ldswc/Form/FormElement.js"></script>
+<script type="module" src="./include/ldswc/Form/FormElementLabel.js"></script>
+<script type="module" src="./include/ldswc/Form/FormElementControl.js"></script>
+<script type="module" src="./include/ldswc/Form/FormElementError.js"></script>
+<script type="module" src="./include/ldswc/Form/InputRaw.js"></script>
+<script type="module" src="./include/ldswc/Form/Input.js"></script>
+<script type="module" src="./include/ldswc/Form/Slider.js"></script>
+<script type="module" src="./include/ldswc/Form/Textarea.js"></script>
+<script type="module" src="./include/ldswc/Form/Select.js"></script>
+<script type="module" src="./include/ldswc/Form/RadioRaw.js"></script>
+<script type="module" src="./include/ldswc/Form/Radio.js"></script>
+<script type="module" src="./include/ldswc/Form/RadioGroup.js"></script>
+<script type="module" src="./include/ldswc/Form/CheckboxRaw.js"></script>
+<script type="module" src="./include/ldswc/Form/Checkbox.js"></script>
+<script type="module" src="./include/ldswc/Form/CheckboxGroup.js"></script>
+<script type="module" src="./include/ldswc/Tabs/Tabs.js"></script>
+<script type="module" src="./include/ldswc/Tabs/Tab.js"></script>
+<script type="module" src="./include/ldswc/Tabs/TabLink.js"></script>
+<script type="module" src="./include/ldswc/vaadingrid/vaadingrid.js"></script>
